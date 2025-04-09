5849390ddf0bc04887c80406416ed4030b9a60b1
jiseolyun <tjfdbs02@gmail.com>
2025-04-09 16:51:56 +0900

    LW 교수님 코드 추가가

--- RISC_V_JI.srcs/sources_1/new/ControlUnit.sv
+++ RISC_V_JI.srcs/sources_1/new/ControlUnit.sv
@@ -7,22 +7,24 @@ module ControlUnit (
     output logic        regFileWe,
     output logic [ 3:0] aluControl,
     output logic        aluSrcMuxSel,
-    output logic        dataWe
+    output logic        dataWe,
+    output logic        RFWDSrcMuxSel
 );
     wire [6:0] opcode = instrCode[6:0];
     wire [3:0] operators = {
         instrCode[30], instrCode[14:12]
     };  // {funct7[5], funct3}
     //regFileWe 읽기 동작할때는 0
-    logic [2:0] signals;
-    assign {regFileWe, aluSrcMuxSel, dataWe, RAMSrcMuxSel} = signals;
+    logic [3:0] signals;
+    assign {regFileWe, aluSrcMuxSel, dataWe, RFWDSrcMuxSel} = signals;
     always_comb begin
         signals = 4'b0;
         case (opcode)
-        // {regFileWe, aluSrcMuxSel, dataWe} = signals;
+            // {regFileWe, aluSrcMuxSel, dataWe, RFWDSrcMuxSel} = signals;
             `OP_TYPE_R: signals = 4'b1_0_0_0;
             `OP_TYPE_S: signals = 4'b0_1_1_0;
             `OP_TYPE_L: signals = 4'b1_1_0_1;
+            //`OP_TYPE_I: signals = 4'b;
         endcase
     end
 
@@ -31,6 +33,7 @@ module ControlUnit (
         case (opcode)
             `OP_TYPE_R: aluControl = operators;  // {funct7[5], funct3}
             `OP_TYPE_S: aluControl = `ADD;
+            `OP_TYPE_L: aluControl = `ADD;
         endcase
     end
 endmodule
